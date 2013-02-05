13c44bd468aff146f609eac25cf2b191de57942f
Sandro Padin <sandropadin@gmail.com>
2013-02-05 13:55:27 -0600

    Trying to get used to the syntax.

--- spec/ticlj/board_state_spec.clj
+++ spec/ticlj/board_state_spec.clj
@@ -3,16 +3,20 @@
 
 (describe "ticlj.board-state"
   (before (reset-board))
+
   (it "returns nil when board index is not occupied"
     (should= " "
              (mark-at-index 0)))
+
   (it "returns x when player x has selected that index"
     (set-mark-at-index x-mark 0)
     (should= x-mark
              (mark-at-index 0)))
+
   (it "return o when player o has selected that index"
     (set-mark-at-index o-mark 0)
     (should= o-mark
              (mark-at-index 0)))
+
   (it "throws an error when mark is not recognized"
     (should-throw Exception (set-mark-at-index "y" 0))))

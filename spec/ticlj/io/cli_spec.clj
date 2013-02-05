705ea60f12ffe89924083ba52667dc3d28a7954e
Sandro Padin <sandropadin@gmail.com>
2013-02-05 16:17:35 -0600

    Adding IO mechanism for Tic Tac Toe game.

---
+++ spec/ticlj/io/cli_spec.clj
@@ -0,0 +1,27 @@
+(ns ticlj.io.cli-spec
+  (:use [speclj.core] [ticlj.io.cli])
+  (:require [ticlj.model.board :as board]))
+
+; helps with passing multiple inputs when using with-in-str
+; http://mikeebert.tumblr.com/post/32243344470/mocking-input-in-clojure-thanks-colin
+(defn make-input [coll]
+  (apply str (interleave coll (repeat "\n"))))
+
+(describe "ticlj.printer.cli"
+  (before (board/reset-board))
+
+  (it "prompts player x for a move"
+    (should= "Player x what is your move?\n"
+             (with-out-str (with-in-str "0" (prompt-player board/x-mark)))))
+
+  (it "prompts player o for a move"
+    (should= "Player o what is your move?\n"
+             (with-out-str (with-in-str "0" (prompt-player board/o-mark)))))
+
+  (it "rejects invalid input until a valid input is entered"
+    (with-out-str (with-in-str (make-input '("bad input" "0"))
+      (should= 0 (prompt-player "x")))))
+
+  (it "prints an empty board"
+    (should= "   |   |   \n---|---|---\n   |   |   \n---|---|---\n   |   |   \n"
+             (with-out-str (print-board board/board)))))
---
+++ src/ticlj/io/cli.clj
@@ -0,0 +1,30 @@
+(ns ticlj.io.cli
+  (:require [ticlj.model.board :as board]))
+
+(def separator "---|---|---")
+
+(defn line-string [a b c]
+  (str " "
+       (board/mark-at-index a)
+       " | "
+       (board/mark-at-index b)
+       " | "
+       (board/mark-at-index c)
+       " "))
+
+(defn print-board [board]
+  (println (line-string 0 1 2))
+  (println separator)
+  (println (line-string 3 4 5))
+  (println separator)
+  (println (line-string 6 7 8)))
+
+(defn prompt-player-recur [mark]
+  (println (str "Player " mark " what is your move?"))
+  (try (Integer/parseInt (read-line))
+    (catch Exception e
+      (println "Invalid input, please try again.")
+      #(prompt-player-recur mark))))
+
+(defn prompt-player [mark]
+  (trampoline prompt-player-recur mark))

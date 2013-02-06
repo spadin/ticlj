9d7388e7bbb38e0f093691a2e0a6301c7a5defd4
Sandro Padin <sandropadin@gmail.com>
2013-02-06 08:59:11 -0600

    Naming ticlj.io.cli as printer locally.
    This may help later if I want to swap out the printer.

--- src/ticlj/game.clj
+++ src/ticlj/game.clj
@@ -1,6 +1,6 @@
 (ns ticlj.game
   (:require [ticlj.model.board :as board]
-            [ticlj.io.cli :as cli]))
+            [ticlj.io.cli :as printer]))
 
 (defn next-player [mark]
   (if (= board/x-mark mark) board/o-mark board/x-mark))
@@ -8,8 +8,8 @@
 (defn play-recur
   ([] (play-recur board/x-mark))
   ([mark]
-    (cli/print-board)
-    (board/set-mark-at-index mark (cli/prompt-player mark))
+    (printer/print-board)
+    (board/set-mark-at-index mark (printer/prompt-player mark))
     (recur (next-player mark))))
 
 (defn start-game []

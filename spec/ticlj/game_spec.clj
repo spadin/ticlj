db12c73b0b3532d0e2fd3b71b83b9a8aff11521c
Sandro Padin <sandropadin@gmail.com>
2013-02-06 15:36:21 -0600

    Refactoring test to reuse some code

--- spec/ticlj/game_spec.clj
+++ spec/ticlj/game_spec.clj
@@ -2,6 +2,32 @@
   (:use [speclj.core] [ticlj.game])
   (:require [ticlj.model.board :as board]))
 
+(defn play-tied-game []
+  (board/set-mark-at-index board/x-mark 0)
+  (board/set-mark-at-index board/o-mark 1)
+  (board/set-mark-at-index board/x-mark 2)
+  (board/set-mark-at-index board/o-mark 5)
+  (board/set-mark-at-index board/x-mark 3)
+  (board/set-mark-at-index board/o-mark 6)
+  (board/set-mark-at-index board/x-mark 4)
+  (board/set-mark-at-index board/o-mark 8)
+  (board/set-mark-at-index board/x-mark 7))
+
+(defn play-x-winning-game []
+  (board/set-mark-at-index board/x-mark 0)
+  (board/set-mark-at-index board/o-mark 3)
+  (board/set-mark-at-index board/x-mark 1)
+  (board/set-mark-at-index board/o-mark 4)
+  (board/set-mark-at-index board/x-mark 2))
+
+(defn play-o-winning-game []
+  (board/set-mark-at-index board/x-mark 0)
+  (board/set-mark-at-index board/o-mark 3)
+  (board/set-mark-at-index board/x-mark 1)
+  (board/set-mark-at-index board/o-mark 4)
+  (board/set-mark-at-index board/x-mark 7)
+  (board/set-mark-at-index board/o-mark 5))
+
 (describe "ticlj.game"
   (before (board/reset-board))
 
@@ -12,25 +38,17 @@
              (next-player board/o-mark)))
 
   (it "determines that x is the winner"
-    (board/set-mark-at-index board/x-mark 0)
-    (board/set-mark-at-index board/x-mark 1)
-    (board/set-mark-at-index board/x-mark 2)
+    (play-x-winning-game)
     (should= board/x-mark
              (winner)))
 
   (it "determines that o is the winner"
-    (board/set-mark-at-index board/o-mark 0)
-    (board/set-mark-at-index board/o-mark 1)
-    (board/set-mark-at-index board/o-mark 2)
+    (play-o-winning-game)
     (should= board/o-mark
              (winner)))
 
   (it "determines that there is a winner"
-    (board/set-mark-at-index board/x-mark 0)
-    (board/set-mark-at-index board/o-mark 3)
-    (board/set-mark-at-index board/x-mark 1)
-    (board/set-mark-at-index board/o-mark 4)
-    (board/set-mark-at-index board/x-mark 2)
+    (play-x-winning-game)
     (should (winner?)))
 
   (it "determines that there is no winner"
@@ -42,23 +60,11 @@
              (gameover?)))
 
   (it "should know the game is over when there is a winner"
-    (board/set-mark-at-index board/x-mark 0)
-    (board/set-mark-at-index board/o-mark 3)
-    (board/set-mark-at-index board/x-mark 1)
-    (board/set-mark-at-index board/o-mark 4)
-    (board/set-mark-at-index board/x-mark 2)
+    (play-x-winning-game)
     (should= true
              (gameover?)))
 
   (it "should know the game is over when the board is full"
-    (board/set-mark-at-index board/x-mark 0)
-    (board/set-mark-at-index board/o-mark 1)
-    (board/set-mark-at-index board/x-mark 2)
-    (board/set-mark-at-index board/o-mark 5)
-    (board/set-mark-at-index board/x-mark 3)
-    (board/set-mark-at-index board/o-mark 6)
-    (board/set-mark-at-index board/x-mark 4)
-    (board/set-mark-at-index board/o-mark 8)
-    (board/set-mark-at-index board/x-mark 7)
+    (play-tied-game)
     (should= true
              (gameover?))))

df21d60004e762df8909d2257c70841e649d872d
Sandro Padin <sandropadin@gmail.com>
2013-02-18 15:01:31 -0600

    Pass max-depth and stop calculating when reached

--- spec/ticlj/model/player/unbeatable_spec.clj
+++ spec/ticlj/model/player/unbeatable_spec.clj
@@ -8,23 +8,39 @@
   (:import [ticlj.model.player.unbeatable UnbeatableAI]))
 
 (describe "ticlj.game.model.player.unbeatable"
+  (it "never returns true when max-depth is nil"
+    (should= false
+             (max-depth-reached? 1 nil)))
+
+  (it "can identify max-depth has been reached"
+    (should= true
+             (max-depth-reached? 2 1)))
+
+  (it "can identify max-depth hasn't been reached"
+    (should= false
+             (max-depth-reached? 1 2)))
+
   (it "returns a score of 1000 for winning at depth 0"
     (should= 1000
-             (calculate-score board/x-mark x-winning-game-board 0)))
+             (calculate-score board/x-mark x-winning-game-board 0 0)))
 
   (it "returns a score of 0 for tied game"
     (should= 0
-             (calculate-score board/x-mark tied-game-board 0)))
+             (calculate-score board/x-mark tied-game-board 0 0)))
 
   (it "returns a score of -1000 for losing game at 0 depth"
     (should= -1000
-             (calculate-score board/x-mark o-winning-game-board 0)))
+             (calculate-score board/x-mark o-winning-game-board 0 0)))
+
+  (it "returns the ab-value when game is not over"
+    (should= -9999
+             (calculate-score board/x-mark board/empty-board 0 -9999)))
 
   (it "returns the move the fills the board"
     (let [board [board/x-mark board/x-mark board/o-mark
                  board/o-mark board/o-mark board/x-mark
                  board/x-mark board/nomark board/o-mark]
-          actual (alpha-beta board/x-mark board)]
+          actual (alpha-beta board/x-mark board nil)]
       (should= 7
               actual)))
 
@@ -32,19 +48,19 @@
     (let [board [board/x-mark board/o-mark board/x-mark
                  board/nomark board/o-mark board/nomark
                  board/o-mark board/nomark board/x-mark]
-          actual (alpha-beta board/x-mark board)]
+          actual (alpha-beta board/x-mark board nil)]
       (should= 5
                actual)))
 
   (context "slow running tests" (tags :slow-tests)
     (it "returns the best first move"
       (should= 0
-              (alpha-beta board/x-mark board/empty-board)))
+              (alpha-beta board/x-mark board/empty-board nil)))
 
     (it "returns the best second move"
       (let [board [board/x-mark board/nomark board/nomark
                    board/nomark board/nomark board/nomark
                    board/nomark board/nomark board/nomark]
-            actual (alpha-beta board/o-mark board)]
+            actual (alpha-beta board/o-mark board nil)]
         (should= 4
                  actual)))))
--- src/ticlj/model/player/unbeatable.clj
+++ src/ticlj/model/player/unbeatable.clj
@@ -11,7 +11,7 @@
 (defrecord UnbeatableAI [mark]
   Player
   (move [this board]
-    (alpha-beta (:mark this) board)))
+    (alpha-beta (:mark this) board nil)))
 
 (defn max-mark-winner? [max-mark board]
   (= (rules/winner board) max-mark))
@@ -19,15 +19,17 @@
 (defn min-mark-winner? [max-mark board]
   (= (rules/winner board) (rules/next-player max-mark)))
 
-(defn calculate-score [max-mark board depth]
-  (if (max-mark-winner? max-mark board)
-      (- 1000 depth)
-      (if (min-mark-winner? max-mark board)
-          (+ -1000 depth)
-          (- 0 depth))))
+(defn calculate-score [max-mark board depth ab-value]
+  (if (rules/gameover? board)
+    (if (max-mark-winner? max-mark board)
+        (- 1000 depth)
+        (if (min-mark-winner? max-mark board)
+            (+ -1000 depth)
+            (- 0 depth)))
+    ab-value))
 
-(defn alpha-beta [max-mark board]
-  (:index (max-value max-mark max-mark board -9999 9999 0)))
+(defn alpha-beta [max-mark board max-depth]
+  (:index (max-value max-mark max-mark board -9999 9999 0 max-depth)))
 
 (defn determine-best-move [current-best test-move index max-node?]
   (let [best-move (if (nil? current-best)
@@ -39,9 +41,12 @@
                               current-best)))]
     best-move))
 
-(defn max-value [max-mark current-mark board alpha beta depth]
-  (if (rules/gameover? board)
-      {:score (calculate-score max-mark board depth)}
+(defn max-depth-reached? [depth max-depth]
+  (and (not (nil? max-depth)) (> depth max-depth)))
+
+(defn max-value [max-mark current-mark board alpha beta depth max-depth]
+  (if (or (rules/gameover? board) (max-depth-reached? depth max-depth))
+      {:score (calculate-score max-mark board depth alpha)}
       (loop [empty-indices (board/get-empty-indices board)
              best-move nil
              alpha alpha beta beta]
@@ -51,7 +56,7 @@
                                                  index
                                                  board)
               other-indices (rest empty-indices)
-              move (min-value max-mark next-mark new-board alpha beta (inc depth))
+              move (min-value max-mark next-mark new-board alpha beta (inc depth) max-depth)
               best-move (determine-best-move best-move move index true)
               alpha (max alpha (:score best-move))
               cut-off? (> alpha beta)]
@@ -61,9 +66,9 @@
                              alpha beta)
                       (merge best-move {:alpha alpha}))))))
 
-(defn min-value [max-mark current-mark board alpha beta depth]
-  (if (rules/gameover? board)
-      {:score (calculate-score max-mark board depth)}
+(defn min-value [max-mark current-mark board alpha beta depth max-depth]
+  (if (or (rules/gameover? board) (max-depth-reached? depth max-depth))
+      {:score (calculate-score max-mark board depth beta)}
       (loop [empty-indices (board/get-empty-indices board)
              best-move nil
              alpha alpha beta beta]
@@ -73,7 +78,7 @@
                                                  index
                                                  board)
               other-indices (rest empty-indices)
-              move (max-value max-mark next-mark new-board alpha beta (inc depth))
+              move (max-value max-mark next-mark new-board alpha beta (inc depth) max-depth)
               best-move (determine-best-move best-move move index false)
               beta (min beta (:score best-move))
               cut-off? (> alpha beta)]

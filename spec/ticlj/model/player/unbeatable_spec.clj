a7bba7be1aa873109e3a6020d7add43309dc82f1
Sandro Padin <sandropadin@gmail.com>
2013-02-18 08:40:22 -0600

    [WIP] trying to get alpha-beta pruning working

--- spec/ticlj/model/player/unbeatable_spec.clj
+++ spec/ticlj/model/player/unbeatable_spec.clj
@@ -9,72 +9,88 @@
 
 (describe "ticlj.game.model.player.unbeatable"
   (it "returns a score of 1 for winning"
-    (should= 1
-             (calculate-score board/x-mark x-winning-game-board)))
+    (should= 1000
+             (calculate-score board/x-mark x-winning-game-board 0)))
 
   (it "returns a score of 0 for tied game"
     (should= 0
-             (calculate-score board/x-mark tied-game-board)))
+             (calculate-score board/x-mark tied-game-board 0)))
 
   (it "returns a score of -1 for losing game"
-    (should= -1
-             (calculate-score board/x-mark o-winning-game-board)))
+    (should= -1000
+             (calculate-score board/x-mark o-winning-game-board 0)))
 
-  (context "alpha-beta pruning" (tags :ab)
-    (it "returns a score of 0 for a tied game"
-      (should= 0
-               (alpha-beta-score board/x-mark tied-game-board 0)))
-
-    (it "returns a score of 999 for a tied game"
-      (should= 999
-               (alpha-beta-score board/x-mark x-winning-game-board 1)))
-
-    (it "returns a move for a tied game"
-      (let [board [board/x-mark board/x-mark board/o-mark
-                   board/o-mark board/o-mark board/x-mark
-                   board/x-mark board/nomark board/o-mark]
-            actual (alpha-beta board/x-mark board/x-mark board 0 -9999 9999 nil)]
-        (should= 7
-                 (:position (reduce (fn [memo val]
-                                      (if (> (:score val) (:score memo))
-                                        val
-                                        memo)) (:scores actual))))))
-
-    (it "returns a move to win game"
-      (let [board [board/x-mark board/o-mark board/x-mark
-                   board/nomark board/o-mark board/nomark
-                   board/o-mark board/nomark board/x-mark]
-            actual (alpha-beta board/x-mark board/x-mark board 0 -9999 9999 nil)]
-        (should= 5
-                 (:position (reduce (fn [memo val]
-                                      (if (> (:score val) (:score memo))
-                                        val
-                                        memo)) (:scores actual)))))))
-
-  (it "returns max move attributes for a tie game"
+  (it "returns the move the fills the board"
     (let [board [board/x-mark board/x-mark board/o-mark
                  board/o-mark board/o-mark board/x-mark
-                 board/x-mark board/nomark board/o-mark]]
+                 board/x-mark board/nomark board/o-mark]
+          actual (alpha-beta board/x-mark board)]
       (should= 7
-               (:position (max-move board/x-mark board 0)))))
+              actual)))
 
-  (it "returns max move x can make for the win"
-    (let [board [board/x-mark board/nomark board/x-mark
-                 board/nomark board/nomark board/o-mark
-                 board/nomark board/o-mark board/nomark]]
-      (should= 1
-               (:position (max-move board/x-mark board 0)))))
-
-  (it "returns max move o can make"
-    (let [board [board/x-mark board/o-mark board/nomark
+  (it "returns a move to win game"
+    (let [board [board/x-mark board/o-mark board/x-mark
                  board/nomark board/o-mark board/nomark
-                 board/o-mark board/x-mark board/x-mark]]
-      (should= 2
-               (:position (max-move board/x-mark board 0)))))
+                 board/o-mark board/nomark board/x-mark]
+          actual (alpha-beta board/x-mark board)]
+      (should= 5
+               actual))))
 
-  (it "returns an index for a move"
-    (let [board [board/x-mark board/x-mark board/o-mark
-                 board/o-mark board/o-mark board/x-mark
-                 board/x-mark board/nomark board/o-mark]]
-      (should= 7
-               (player/move (UnbeatableAI. board/x-mark) board)))))
+  ;(context "alpha-beta pruning" (tags :ab)
+    ;(it "returns a score of 0 for a tied game"
+      ;(should= 0
+               ;(alpha-beta-score board/x-mark tied-game-board 0)))
+
+    ;(it "returns a score of 999 for a tied game"
+      ;(should= 999
+               ;(alpha-beta-score board/x-mark x-winning-game-board 1)))
+
+    ;(it "returns a move for a tied game"
+      ;(let [board [board/x-mark board/x-mark board/o-mark
+                   ;board/o-mark board/o-mark board/x-mark
+                   ;board/x-mark board/nomark board/o-mark]
+            ;actual (alpha-beta board/x-mark board/x-mark board 0 -9999 9999 nil)]
+        ;(should= 7
+                 ;(:position (reduce (fn [memo val]
+                                      ;(if (> (:score val) (:score memo))
+                                        ;val
+                                        ;memo)) (:scores actual))))))
+
+    ;(it "returns a move to win game"
+      ;(let [board [board/x-mark board/o-mark board/x-mark
+                   ;board/nomark board/o-mark board/nomark
+                   ;board/o-mark board/nomark board/x-mark]
+            ;actual (alpha-beta board/x-mark board/x-mark board 0 -9999 9999 nil)]
+        ;(should= 5
+                 ;(:position (reduce (fn [memo val]
+                                      ;(if (> (:score val) (:score memo))
+                                        ;val
+                                        ;memo)) (:scores actual)))))))
+
+  ;(it "returns max move attributes for a tie game"
+    ;(let [board [board/x-mark board/x-mark board/o-mark
+                 ;board/o-mark board/o-mark board/x-mark
+                 ;board/x-mark board/nomark board/o-mark]]
+      ;(should= 7
+               ;(:position (max-move board/x-mark board 0)))))
+
+  ;(it "returns max move x can make for the win"
+    ;(let [board [board/x-mark board/nomark board/x-mark
+                 ;board/nomark board/nomark board/o-mark
+                 ;board/nomark board/o-mark board/nomark]]
+      ;(should= 1
+               ;(:position (max-move board/x-mark board 0)))))
+
+  ;(it "returns max move o can make"
+    ;(let [board [board/x-mark board/o-mark board/nomark
+                 ;board/nomark board/o-mark board/nomark
+                 ;board/o-mark board/x-mark board/x-mark]]
+      ;(should= 2
+               ;(:position (max-move board/x-mark board 0)))))
+
+  ;(it "returns an index for a move"
+    ;(let [board [board/x-mark board/x-mark board/o-mark
+                 ;board/o-mark board/o-mark board/x-mark
+                 ;board/x-mark board/nomark board/o-mark]]
+      ;(should= 7
+               ;(player/move (UnbeatableAI. board/x-mark) board)))))
--- src/ticlj/model/player/unbeatable.clj
+++ src/ticlj/model/player/unbeatable.clj
@@ -4,112 +4,93 @@
             [ticlj.model.player :as player])
   (:import [ticlj.model.player Player]))
 
-(declare max-move)
-(declare min-move)
 (declare alpha-beta)
+(declare max-value)
+(declare min-value)
+
 (defrecord UnbeatableAI [mark]
   Player
   (move [this board]
-    (:position (reduce (fn [memo val]
-                         (if (> (:score val) (:score memo))
-                           val
-                           memo))
-                       (:scores (alpha-beta board/x-mark
-                                            board/x-mark
-                                            board 0 -9999 9999 nil))))))
-
-(defn calculate-score [mark board]
-  (if (= (rules/winner board) mark)
-    1
-    (if (= (rules/winner board) (rules/next-player mark))
-      -1
-      0)))
+    (alpha-beta (:mark this) board)))
 
 (defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
 
-(defn alpha-beta-score [mark board depth]
-  (if (= (rules/winner board) mark)
-    (- 1000 depth)
-    (if (= (rules/winner board) (rules/next-player mark))
-      (+ -1000 depth)
-      (- 0 depth))))
+(defn max-mark-winner? [max-mark board]
+  (= (rules/winner board) max-mark))
 
-(defn alpha-beta [max-mark current-mark board depth alpha beta scores]
+(defn min-mark-winner? [max-mark board]
+  (= (rules/winner board) (rules/next-player max-mark)))
+
+(defn calculate-score [max-mark board depth]
+  (if (max-mark-winner? max-mark board)
+      (- 1000 depth)
+      (if (min-mark-winner? max-mark board)
+          (+ -1000 depth)
+          (- 0 depth))))
+
+(defn alpha-beta [max-mark board]
+  (:index (max-value max-mark max-mark board -9999 9999 0)))
+
+(defn determine-best-move [current-best test-move index max-node?]
+  (let [best-move (if (nil? current-best)
+                      (merge test-move {:index index})
+                      (if (and max-node? (> (:score test-move) (:score current-best)))
+                          (merge test-move {:index index})
+                          (if (and (not max-node?) (< (:score test-move) (:score current-best)))
+                              (merge test-move {:index index})
+                              current-best)))]
+    best-move))
+
+(defn max-value [max-mark current-mark board alpha beta depth]
   (if (rules/gameover? board)
-    {:score (alpha-beta-score max-mark board depth) :alpha alpha :beta beta :depth depth}
-    (let [positions (board/get-empty-indices board)]
-      (if (= max-mark current-mark) ;;max-player
-        (loop [positions positions
-               alpha alpha
-               beta beta
-               cut-off? false
-               scores []]
-          (let [position (first positions)
-                score (:score (alpha-beta max-mark
-                                  (rules/next-player current-mark)
-                                  (board/set-mark-at-index current-mark position board)
-                                  (inc depth)
-                                  alpha beta scores))
-                next-alpha (if (> score alpha) score alpha)
-                next-cut-off (>= next-alpha beta)
-                scores (cons {:score score
-                              :position position
-                              :alpha next-alpha
-                              :beta beta} scores)]
-            (if (and (not cut-off?) (not (empty? (rest positions))))
-              (recur (rest positions)
-                     next-alpha beta next-cut-off scores)
-              {:score score
-               :alpha next-alpha
-               :beta beta
-               :position position
-               :depth depth
-               :scores scores})))
-        (loop [positions positions
-               alpha alpha
-               beta beta
-               cut-off? false
-               scores []]
-          (let [position (first positions)
-                score (:score (alpha-beta max-mark
-                                  (rules/next-player current-mark)
-                                  (board/set-mark-at-index current-mark position board)
-                                  (inc depth)
-                                  alpha beta scores))
-                next-beta (if (< score beta) score beta)
-                next-cut-off (>= alpha next-beta)
-                scores (cons {:score score
-                              :position position
-                              :alpha alpha
-                              :beta next-beta} scores)]
-            (if (and (not cut-off?) (not (empty? (rest positions))))
-              (recur (rest positions) alpha next-beta next-cut-off scores)
-              {:score score
-               :alpha alpha
-               :beta next-beta
-               :position position
-               :depth depth
-               :scores scores})))))))
+      {:score (calculate-score max-mark board depth)}
+      (loop [empty-indices (board/get-empty-indices board)
+             best-move nil
+             alpha alpha
+             beta beta
+             cut-off? false]
+        (let [index (first empty-indices)
+              next-mark (rules/next-player current-mark)
+              new-board (board/set-mark-at-index current-mark
+                                                 index
+                                                 board)
+              other-indices (rest empty-indices)
+              move (min-value max-mark next-mark new-board alpha beta (inc depth))
+              next-best-move (determine-best-move best-move move index true)
+              next-alpha (max alpha (:score next-best-move))
+              next-beta beta
+              next-cut-off? (> next-alpha next-beta)]
+              (if-not (and (not cut-off?) (empty? other-indices))
+                      (recur other-indices
+                             next-best-move
+                             next-alpha
+                             next-beta
+                             cut-off?)
+                             next-best-move)))))
 
-
-(defn min-move [mark board depth]
-  (let [moves
-        (for [position (board/get-empty-indices board)
-              :let [new-board (board/set-mark-at-index mark position board)]]
-          (if (rules/gameover? new-board)
-            {:score (+ (- -1000 (calculate-score (rules/next-player mark) new-board)) depth)
-             :position position}
-            {:score (:score (max-move (rules/next-player mark) new-board (inc depth)))
-             :position position}))]
-    (reduce (fn [memo val]  (if (< (:score val) (:score memo)) val memo)) moves)))
-
-(defn max-move [mark board depth]
-  (let [moves
-        (for [position (board/get-empty-indices board)
-              :let [new-board (board/set-mark-at-index mark position board)]]
-          (if (rules/gameover? new-board)
-            {:score (- 1000 (calculate-score mark new-board) depth)
-             :position position}
-            {:score (:score (min-move (rules/next-player mark) new-board (inc depth)))
-             :position position}))]
-    (reduce (fn [memo val]  (if (> (:score val) (:score memo)) val memo)) moves)))
+(defn min-value [max-mark current-mark board alpha beta depth]
+  (if (rules/gameover? board)
+      {:score (calculate-score max-mark board depth)}
+      (loop [empty-indices (board/get-empty-indices board)
+             best-move nil
+             alpha alpha
+             beta beta
+             cut-off? false]
+        (let [index (first empty-indices)
+              next-mark (rules/next-player current-mark)
+              new-board (board/set-mark-at-index current-mark
+                                                 index
+                                                 board)
+              other-indices (rest empty-indices)
+              move (max-value max-mark next-mark new-board alpha beta (inc depth))
+              next-best-move (determine-best-move best-move move index false)
+              next-alpha alpha
+              next-beta (min beta (:score next-best-move))
+              next-cut-off? (> next-alpha next-beta)]
+              (if-not (and (not cut-off?) (empty? other-indices))
+                      (recur other-indices
+                             next-best-move
+                             next-alpha
+                             next-beta
+                             cut-off?)
+                             next-best-move)))))

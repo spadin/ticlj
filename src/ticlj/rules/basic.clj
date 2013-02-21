4b047b0ab0a854e2b40742035fe9e0873d239506
Sandro Padin <sandropadin@gmail.com>
2013-02-21 14:29:24 -0600

    winning-combinations use multimethod

--- src/ticlj/rules/basic.clj
+++ src/ticlj/rules/basic.clj
@@ -1,12 +1,19 @@
 (ns ticlj.rules.basic
-  (:require [ticlj.board.basic :as board]))
+  (:use [ticlj.rules.game-type :only (*game-type*)])
+  (:require [ticlj.board.basic :as board]
+            [ticlj.rules.game-type :as game-type]))
 
-(def winning-combinations [#{0 1 2} #{3 4 5} #{6 7 8}
-                           #{0 3 6} #{1 4 7} #{2 5 8}
-                           #{0 4 8} #{2 4 6}])
+(defmulti multi-winning-combinations :game-type)
+(defmethod multi-winning-combinations game-type/basic [this]
+  [#{0 1 2} #{3 4 5} #{6 7 8}
+   #{0 3 6} #{1 4 7} #{2 5 8}
+   #{0 4 8} #{2 4 6}])
+
+(defn winning-combinations []
+  (multi-winning-combinations {:game-type *game-type*}))
 
 (defn is-winner? [mark board]
-  (some #(= (set (filter % (board/get-moves mark board))) %) winning-combinations))
+  (some #(= (set (filter % (board/get-moves mark board))) %) (winning-combinations)))
 
 (defn winner [board]
   (if (is-winner? board/x-mark board)

ea65c222c68707ea05807ca10c8fc7c14378502e
Sandro Padin <sandropadin@gmail.com>
2013-03-06 10:02:59 -0600

    Moving speclj into dev dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject org.clojars.sandropadin/ticlj "0.1.0-SNAPSHOT"
   :description "Tic Tac Toe Game in Clojure"
   :url "https://github.com/spadin/ticlj"
-  :dependencies [[org.clojure/clojure "1.5.0"]
-                 [speclj "2.5.0"]]
+  :dependencies [[org.clojure/clojure "1.5.0"]]
   :plugins [[speclj "2.5.0"]
             [lein-marginalia "0.7.1"]]
   :test-paths ["spec/"]
-  :profiles {:dev {:dependencies [[lein-marginalia "0.7.1"]]}}
+  :profiles {:dev {:dependencies [[lein-marginalia "0.7.1"]
+                                  [speclj "2.5.0"]]}}
   :main ticlj.core)

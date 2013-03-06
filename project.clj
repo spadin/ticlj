e2a8a436563121d232dc3e3a2a40a4d52f9415a5
Sandro Padin <sandropadin@gmail.com>
2013-03-06 09:59:11 -0600

    Updating project name and Clojure version.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject ticlj "0.1.0-SNAPSHOT"
+(defproject org.clojars.sandropadin/ticlj "0.1.0-SNAPSHOT"
   :description "Tic Tac Toe Game in Clojure"
   :url "https://github.com/spadin/ticlj"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [speclj "2.5.0"]]
   :plugins [[speclj "2.5.0"]
             [lein-marginalia "0.7.1"]]
